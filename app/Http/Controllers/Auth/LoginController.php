baa471942485ccb0fc4e8787d41647a2e2c9d954
SZLforGithub <i0989872540@gmail.com>
2018-12-20 17:23:21 +0800

    登出

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Auth;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -32,6 +34,11 @@ class LoginController extends Controller
      *
      * @return void
      */
+    public function logout(Request $request) {
+        Auth::logout();
+        return redirect('/login');
+    }
+
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
