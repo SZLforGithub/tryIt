10d288c0673ac5707666f7bbd87ddffa69d68f48
SZLforGithub <i0989872540@gmail.com>
2018-12-20 17:21:58 +0800

    Test button trigger

---
+++ app/Http/Controllers/shotController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class shotController extends Controller
+{
+    public function uploadShot(Request $request)
+    {
+    	dd("Fuck you!!");
+    }
+}
