0e0ee2ecf8d431a71f1458f8f817cdde940cf29f
SZLforGithub <i0989872540@gmail.com>
2019-01-04 10:29:33 +0800

    provider做數據在視圖的共享
    原本想用view()->share()，但Auth::check()在boot會回傳false，原因和Middleware seesion的存取順序有關，所以用了一個trick代替

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\photo;
+use App\User;
+use Auth;
 use Schema;
 
 class AppServiceProvider extends ServiceProvider
@@ -15,8 +18,26 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        //將smallSource傳到app.blade
+        view()->composer('layouts.app', function($view) {
+                $view->with('smallSource', $this->getVariable());
+         });
     }
 
+    private function getVariable()
+    {
+        if (Auth::check()) {
+            $whoYouAre = Auth::user()->name;
+            $user = User::where('name', '=', $whoYouAre)->first();
+            $sourcePath = $user->shot_path;
+            $photo = Photo::where('path', '=', $sourcePath)->first();
+            if($photo == null)
+                $smallSource = null;
+            else
+                $smallSource = $photo->smallSource;
+            return $smallSource;
+        }
+    }
     /**
      * Register any application services.
      *
