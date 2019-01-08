4c59c89d8d3eb7d70108ff6f099730a4b3fc258e
SZLforGithub <i0989872540@gmail.com>
2019-01-08 17:37:09 +0800

    文章備份post_backups

---
+++ database/migrations/2019_01_08_100724_create_post_backups_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostBackupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_backups', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('poster');
+            $table->longText('content');
+            $table->string('photos_path')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_backups');
+    }
+}
