23e98e7fbb693f5855758eef0e91133f4f773476
SZLforGithub <i0989872540@gmail.com>
2019-01-29 14:34:43 +0800

    Update shotController.php

--- app/Http/Controllers/shotController.php
+++ app/Http/Controllers/shotController.php
@@ -11,30 +11,35 @@ use Image;
 
 class shotController extends Controller
 {
+	public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function uploadShot(Request $request)
     {
     	if($request->isMethod('post')){
     		$shot = $request->file('shot');
     		if($shot->isValid()){
-    			//獲取Shot相關資訊
+    			// 獲取Shot相關資訊
     			$originalName = $shot->getClientOriginalName();
     			$ext = $shot->getClientOriginalExtension();
     			$realPath = $shot->getRealPath();
     			$type = $shot->getClientMimeType();
 
-    			//上傳
+    			// 上傳
     			$filename = date('Y-m-d-H-i-s') . '-' . uniqid() . '.' .$ext;
     			$path = Storage::putfileAs('public', $shot, $filename);
     			//dd(Storage::url($path));
 
     			$sourcePath = Storage::url($path);
     			$whoYouAre = Auth::user()->name;
-    			//存入資料庫
+    			// 存入資料庫
     			$photo = new photo;
     			$photo->path = $sourcePath;
     			$photo->user = $whoYouAre;
     			$photo->save();
-    			//存入user的shot_path欄位
+    			// 存入user的shot_path欄位
     			$user = User::where('name', '=', $whoYouAre)->first();
     			$user->shot_path = $sourcePath;
     			$user->save();
@@ -54,6 +59,7 @@ class shotController extends Controller
     	$whoYouAre = Auth::user()->name;
     	$user = User::where('name', '=', $whoYouAre)->first();
 
+    	// XXX Maybe this part can do smarter
     	// 去掉url開頭的斜線 才能順利make
     	$sourcePath = $user->shot_path;
     	$path = substr_replace($sourcePath, "", 0, 1);
@@ -66,7 +72,7 @@ class shotController extends Controller
     	$img = Image::make($path)->crop($w, $h, $x1, $y1)->save(public_path().'/'.$editPath);
     	$img = Image::make($editPath)->resize(50, 50)->save(public_path().$smallEditPath);
 
-    	//將裁切後Shot的Path存入Photos資料表的editSource和smallSource
+    	// 將裁切後Shot的Path存入Photos資料表的editSource和smallSource
     	$photo = Photo::where('path', '=', $sourcePath)->first();
     	$photo->editSource = $editPath;
     	$photo->smallSource = $smallEditPath;
