402d0e3086379f3622316571fe46d159f74e04e2
SZLforGithub <i0989872540@gmail.com>
2019-01-29 14:34:53 +0800

    Update ProfileController.php

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -10,6 +10,11 @@ use View;
 
 class ProfileController extends Controller
 {
+	public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function index(Request $request)
     {
     	if(isset(Auth::user()->name)){
